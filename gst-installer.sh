89f027bbbd241c5f6c460270b4cb43660fd68acd
KIR <seiga@frl.mech.tohoku.ac.jp>
2017-10-04 12:38:01 +0900

    オプションの変数化，ldconfigの追加

--- gst-installer.sh
+++ gst-installer.sh
@@ -1,100 +1,126 @@
 #!/bin/bash
-set -e
-
-printf "password: "
-read password
+read -sp "Password: " password
+echo
 
 srcbranch="origin/1.12"
+confflag="--prefix=/usr/local"
+libpath="-L/usr/local/lib/"
+workpath="./gst"
+
+if [ -e ${workpath} ]
+then
+	rm -rf ${workpath}
+fi
 
 mkdir gst
 cd gst
 
+export LIB="$LIB ${libpath}"
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+
+set -e
+
 echo "$password" | sudo -S apt install bison flex autoconf liborc*
+
 git clone https://github.com/GStreamer/gstreamer.git
 cd gstreamer
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+set -x
+./configure ${confflag}
 make -j4
+set +x
 echo "$password" | sudo -S make install
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
+echo "$password" | sudo -S ldconfig
 cd ../
 
 git clone https://github.com/GStreamer/gst-plugins-base.git
 cd gst-plugins-base
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 echo "$password" | sudo -S apt install libjpeg-dev libvpx-dev
 git clone https://github.com/GStreamer/gst-plugins-good.git
 cd gst-plugins-good
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 echo "$password" | sudo -S apt install x265
 git clone https://github.com/GStreamer/gst-plugins-bad.git
 cd gst-plugins-bad
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 echo "$password" | sudo -S apt install libx264-dev libmpg123-dev
 git clone https://github.com/GStreamer/gst-plugins-ugly.git
 cd gst-plugins-ugly
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 git clone https://github.com/GStreamer/gst-libav.git
 cd gst-libav
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 git clone https://anongit.freedesktop.org/git/gstreamer/gst-ffmpeg.git
 cd gst-ffmpeg
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 git clone https://github.com/GStreamer/gst-rtsp-server.git
 cd gst-rtsp-server
 git checkout ${srcbranch}
-./autogen.sh
-./configure
+./autogen.sh --noconfigure
+./configure ${confflag}
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
 
 git clone https://github.com/GStreamer/gst-omx.git
 cd gst-omx
 git checkout ${srcbranch}
-./autogen.sh
-LIBS="$LIBS -L/usr/loca/lib" \
-LDFLAGS='-L/opt/vc/lib -lGLESv2 -lbcm_host -lEGL -lm -lstdc++' \
-CPPFLAGS='-I/opt/vc/include -I/opt/vc/include/IL -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux' \
-./configure \
+export LDFLAGS="-L/opt/vc/lib -lGLESv2 -lbcm_host -lEGL -lm -lstdc++"
+export CPPFLAGS="-I/opt/vc/include -I/opt/vc/include/IL -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux"
+./autogen.sh --noconfigure 
+./configure ${confflag} \
 --with-omx-target=rpi
 make -j4
 echo "$password" | sudo -S make install
+echo "$password" | sudo -S ldconfig
 cd ../
+
+echo "export GST_OMX_CONFIG_DIR=/usr/local/etc/xdg/" >> ~/.bashrc
+echo "export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0/" >> ~/.bashrc
+
+source ~/.bashrc
